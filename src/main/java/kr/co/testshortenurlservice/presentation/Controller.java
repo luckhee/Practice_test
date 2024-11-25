e86da156146858a45c26d56a7a6530ff317f9f12
luckhee <114234435+luckhee@users.noreply.github.com>
2024-11-25 22:10:39 +0900

    연습

--- src/main/java/kr/co/testshortenurlservice/presentation/Controller.java
+++ src/main/java/kr/co/testshortenurlservice/presentation/Controller.java
@@ -4,8 +4,13 @@ package kr.co.testshortenurlservice.presentation;
 import jakarta.validation.Valid;
 import kr.co.testshortenurlservice.application.ShortenUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class Controller {
@@ -25,12 +30,18 @@ public class Controller {
     }
 
     @RequestMapping(value = "/{shortenUrlKey}", method = RequestMethod.GET)
-    public ResponseEntity<?> redirect() {
-        return ResponseEntity.ok().body(null);
+    public ResponseEntity<?> redirect(@PathVariable String shortenUrlKey) throws URISyntaxException {
+        String originalUrl = shortenUrlService.getOriginalUrlByShortenUrl(shortenUrlKey);
+
+        URI redirectUri = new URI(originalUrl);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirectUri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
     @RequestMapping(value = "/shortenUrl/{shortenUrlKey}" , method = RequestMethod.GET)
-    public ResponseEntity<ShortenUrlInformationDto> searchInformation(String shortenUrlKey) {
+    public ResponseEntity<ShortenUrlInformationDto> searchInformation(@PathVariable String shortenUrlKey) {
 
         ShortenUrlInformationDto shortenUrlInformationDto = shortenUrlService.findInformation(shortenUrlKey);
 
