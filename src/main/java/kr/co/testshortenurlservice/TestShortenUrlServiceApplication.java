201a0b986c96f3e7b2fba60977a4d8b1a649a54b
luckhee <114234435+luckhee@users.noreply.github.com>
2024-11-25 19:19:45 +0900

    연습

---
+++ src/main/java/kr/co/testshortenurlservice/TestShortenUrlServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.testshortenurlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShortenUrlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestShortenUrlServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/testshortenurlservice/TestShortenUrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.testshortenurlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShortenUrlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
